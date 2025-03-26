1101a03f567c51b734b00f2da265fb4bb9058fbd
Tony <toshik06rom@rambler.ru>
2025-03-26 22:33:34 +0300

    add new classes and main_16_1

--- src/classes.py
+++ src/classes.py
@@ -97,3 +97,24 @@ class CategoryIterNext:
             return product
         else:
             raise StopIteration
+
+
+class Smartphone(Product):
+    """Расширение класса Product для смартфонов"""
+
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    """Расширение класса Product для газонной травы"""
+
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
